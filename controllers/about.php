e151361b04e1683fff0d9ca0229c47f8477ab266
PhanAnh38 <phananh.main@gmail.com>
2022-12-23 16:56:16 +0700

    all code

--- controllers/about.php
+++ controllers/about.php
@@ -1,9 +1,9 @@
 <?php
 //dieu khien trang home
 
-function show_about(){
+function show_about()
+{
     $danh_muc_top_10 = so_luot_yeu_thich();
 
     render("about", ['danh_muc_top_10' => $danh_muc_top_10]);
 }
-?>
